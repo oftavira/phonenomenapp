8526dc02f7bf249f875d4ed441c815009ab8f16d
oftavira <o.f.tavira@gmail.com>
2021-04-17 10:50:04 -0500

    Subiendo archivos faltantes

---
+++ lib/navigation_service.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppNavigator {
+  final GlobalKey<NavigatorState> navKey = GlobalKey<NavigatorState>();
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+}
